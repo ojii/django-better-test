0fdaa04e535732395746ea389b4981b1a1072c42
Jonas Obrist <ojiidotch@gmail.com>
2015-09-03 00:30:55 +0200

    suppress ReportCrash(8) on OS X during segfault test

--- better_test/tests/test_crash.py
+++ better_test/tests/test_crash.py
@@ -1,3 +1,5 @@
+import contextlib
+import subprocess
 import sys
 import signal
 
@@ -7,21 +9,45 @@ from better_test import core
 from better_test.parallel import SilentMultiProcessingTextTestResult
 from better_test.utils import get_test_runner
 
+if sys.platform == 'darwin':
+    @contextlib.contextmanager
+    def no_report_crash():
+        subprocess.check_call([
+            'launchctl',
+            'unload',
+            '-w',
+            '/System/Library/LaunchAgents/com.apple.ReportCrash.plist'
+        ])
+        try:
+            yield
+        finally:
+            subprocess.check_call([
+                'launchctl',
+                'load',
+                '-w',
+                '/System/Library/LaunchAgents/com.apple.ReportCrash.plist'
+            ])
+else:
+    @contextlib.contextmanager
+    def no_report_crash():
+        yield
+
 
 class TestCrashingExecutors(unittest.TestCase):
     def test_segfault(self):
-        result = core.run(
-            ['better_test.harness.crash.Tests.test_segfault'],
-            {},
-            core.Config(
-                test_runner_class=get_test_runner(),
-                mode=core.PARALLEL,
-                timings={},
-                processes=1,
-                debug=True
-            ),
-            real_result_class=SilentMultiProcessingTextTestResult
-        )
+        with no_report_crash():
+            result = core.run(
+                ['better_test.harness.crash.Tests.test_segfault'],
+                {},
+                core.Config(
+                    test_runner_class=get_test_runner(),
+                    mode=core.PARALLEL,
+                    timings={},
+                    processes=1,
+                    debug=True
+                ),
+                real_result_class=SilentMultiProcessingTextTestResult
+            )
         self.assertFalse(result.success)
         self.assertEqual(len(result.failed_executors), 1)
         _, exit_code = result.failed_executors[0]
@@ -31,3 +57,4 @@ class TestCrashingExecutors(unittest.TestCase):
             self.assertEqual(exit_code, -signal.SIGILL)
         elif sys.platform == 'win32':
             self.assertEqual(exit_code, 3221225477)
+
